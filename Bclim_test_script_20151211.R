195d431fe63551e56b6ff83195afd321cb611030
Andrew Parnell <andrew.parnell@gmail.com>
2017-05-29 20:38:31 +0100

    Update scripts

--- Bclim_test_script_20151211.R
+++ Bclim_test_script_20151211.R
@@ -5,33 +5,25 @@ devtools::install_github('Bclim')
 #install.packages('Bclim')
 library(Bclim)
 
-# Source in new functions if required
-source('../Bclim/R/layer_clouds.R')
-dyn.load(system.file('libs/Bclim.so',package='Bclim'))
-
 # Load in the Sluggan data - pollen and chronologies
 sluggan_pollen = read.table('data/Sluggan_pollen.txt',header=TRUE)
 sluggan_chrons = read.table('data/Sluggan_chrons.txt',nrow=2000)
 
 # Run the layer_clouds function
-source('../Bclim/R/layer_clouds.R')
 layers = layer_clouds(sluggan_pollen)
 
 # Run the climate histories function
-source('../Bclim/R/climate_histories.R')
 histories = climate_histories(layers,sluggan_chrons,seq(0,14,by=0.1))
 save(histories,file='Sluggan_histories.rda')
 #load(file='Sluggan_histories.rda')
 
 # Function plot.layer_clouds
-source('../Bclim/R/plot.layer_clouds.R')
 par(mar=c(3,3,2,1), mgp=c(2,.7,0), tck=-.01,las=1)
 #plot(layers,dims=2:3,main='Layer 1',xlab='MTCO',ylab='AET/PET',col=terrain.colors(30))
 plot(histories$layers,main='Layer 1',xlab='GDD5',ylab='MTCO',col=terrain.colors(30))
 plot(histories$layers,layer=50, dims=2:3, main='Layer 50',xlab='MTCO',ylab='AET/PET',col=terrain.colors(30))
 
 # Function plot.climate_histories
-source('../Bclim/R/plot.climate_histories.R')
 par(mar=c(3,3,2,1), mgp=c(2,.7,0), tck=-.01,las=1)
 plot(histories,chron=sluggan_chrons,xlab='Age (k cal years BP)',ylab='GDD5',main='Sluggan Moss')
 legend('topleft',legend=c('Layer clouds','Climate ribbon','Representative history'),pch=15,col=c('blue','red','green'))
@@ -41,6 +33,5 @@ plot(histories,dim=3,most_representative = 3, chron=sluggan_chrons,xlab='Age (k 
 legend('bottomleft',legend=c('Layer clouds','Climate ribbon','Representative histories'),pch=15,col=c('blue','red','green'))
 
 # Function summary.climate_histories
-source('../Bclim/R/summary.climate_histories.R')
 summary(histories,dim=3)
 
